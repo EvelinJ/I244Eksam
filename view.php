67e37e74d625326b8987b02e4e707e9cc38526f4
Evelin Jõgi <Evelin.Jogi@hm.ee>
2016-06-16 18:25:13 +0300

    Parandasin treppimist.

--- view.php
+++ view.php
@@ -9,19 +9,19 @@
 	
     <body>
 		
-        <h1 class="pealkiri">Sellel lehel kuvatakse lehe külastamise aegu</h1>
+        <h1 class="pealkiri">Sellel lehel kuvatakse lehe külastuste aegu</h1>
 		
         <h2>Lehte on külastatud järgmistel aegadel: </h2>
         <table>
-        <th>Külastamise aeg</th>
-        <?php 
-            foreach ( model_load() as $rida ): ?>
-                <tr>
-                    <td> 
-                        <?= htmlspecialchars($rida['aeg']); ?>
-                    </td>
-                </tr>
-        <?php endforeach; ?>
+            <th>Külastamise aeg</th>
+            <?php 
+                foreach ( model_load() as $rida ): ?>
+                    <tr>
+                        <td> 
+                            <?= htmlspecialchars($rida['aeg']); ?>
+                        </td>
+                    </tr>
+            <?php endforeach; ?>
         </table>
     </body>
 </html>
