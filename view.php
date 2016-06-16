1ccd378f51afc8cf6ca6c25c9cbd0acf904ead30
Evelin Jõgi <Evelin.Jogi@hm.ee>
2016-06-16 18:10:20 +0300

    Lisasin CSS-i.

--- view.php
+++ view.php
@@ -3,23 +3,25 @@
     <head>
         <meta charset="utf-8"/>
         <title>Lehe külastamise ajad</title>
+		<link rel="stylesheet" type="text/css" href="css.css">
+		
     </head>
 	
     <body>
 		
         <h1 class="pealkiri">Sellel lehel kuvatakse lehe külastamise aegu</h1>
 		
-		<h2>Lehte on külastatud järgmistel aegadel: </h2>
-		<table>
-		<th>Külastamise aeg</th>
-		<?php 
+        <h2>Lehte on külastatud järgmistel aegadel: </h2>
+        <table>
+        <th>Külastamise aeg</th>
+        <?php 
             foreach ( model_load() as $rida ): ?>
-				<tr>
-				    <td> 
+                <tr>
+                    <td> 
                         <?= htmlspecialchars($rida['aeg']); ?>
                     </td>
                 </tr>
         <?php endforeach; ?>
-		</table>
+        </table>
     </body>
 </html>
